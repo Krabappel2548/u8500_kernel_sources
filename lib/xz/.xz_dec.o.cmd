243e50b49d0d684e01d1d47ca18cfd33cd3ce108
Wouter Heijligen <krabappel2548@gmail.com>
2012-07-02 22:43:39 +0200

    Sources from stock kernel
    Going to start all over with the kernel, I made some mistakes

---
+++ lib/xz/.xz_dec.o.cmd
@@ -0,0 +1,1 @@
+cmd_lib/xz/xz_dec.o := /home/wouter/arm-eabi-4.4.3/bin/arm-eabi-ld -EL    -r -o lib/xz/xz_dec.o lib/xz/xz_dec_syms.o lib/xz/xz_dec_stream.o lib/xz/xz_dec_lzma2.o lib/xz/xz_dec_bcj.o ; scripts/mod/modpost lib/xz/xz_dec.o
